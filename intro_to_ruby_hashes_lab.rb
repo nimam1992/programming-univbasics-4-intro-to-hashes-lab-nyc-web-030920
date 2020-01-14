c612878be60ba61b8c82201badf161ba08518fe3
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-01-14 00:33:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,10 @@
 def new_hash
-  hash = hash.new
+  hash = {}
   return hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  hash = {'monkey':'playpin'}
 end
 
 def pioneer
