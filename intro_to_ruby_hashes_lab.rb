c824801aab5374d88d6feeb9968d9e811c3704c8
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-01-14 01:21:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,8 +30,10 @@ def update_counting_hash(hash, key)
   hash
   if hash[key]
     hash[key]+=1
+    return hash
   else
     hash[key]=1
+    return hash
   end
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
